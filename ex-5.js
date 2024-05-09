2d3659ae36e761a87c934021c13c242a971a919c
Ben Wattanasup <benwata777@gmail.com>
2024-05-09 15:04:55 +0700

    java

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,8 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let price = 27000;
+let discountPercent = 25;
+let discount = (discountPercent / 100) * price;
+let totalPrice = price - discount;
+console.log(totalPrice);
